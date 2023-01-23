3f3e11c3d16911f4d6f1c172f688ac6e97ea4d4f
chidiebereojingwa <ojingwaoji@gmail.com>
2023-01-23 06:08:41 +0100

    worked on stack

--- 02_02_begin/stack.py
+++ 02_02_begin/stack.py
@@ -6,4 +6,23 @@ Robin Andrews - https://compucademy.net/
 
 
 class Stack:
-    pass
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        # return len(self.items) == 0
+        return not self.items
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+    
+    def __str__(self):
